f8a6ec4c5b7deca7c9aa5d622021f7db0a2fb958
BombayV <OMGmsebastian@gmail.com>
2022-03-13 14:39:35 -0400

    feat(appearance): added callback function

--- client/main.lua
+++ client/main.lua
@@ -1,25 +1,32 @@
 ModelNames = {}
 OldSkin = {}
+Callback = nil
 
-local function startAppearance(config)
-	SetNuiFocus(true, true)
-	local data = GetData(config)
-	local skin = GetSkin(PlayerPedId())
-	SendNUIMessage({
-		action = "skin_start",
-		skin = skin,
-		config = data.config,
-		settings = data.settings,
-		colors = data.colors,
-		freeMode = IsFreemode(GetEntityModel(PlayerPedId())),
-	})
-	FreezeEntityPosition(PlayerPedId(), true)
-	OldSkin = skin
-	Cam.Create(PlayerPedId())
+local function startAppearance(config, cb)
+  SetNuiFocus(true, true)
+  local data = GetData(config)
+  local skin = GetSkin(PlayerPedId())
+  SendNUIMessage {
+    action = 'skin_start',
+    skin = skin,
+    config = data.config,
+    settings = data.settings,
+    colors = data.colors,
+    freeMode = IsFreemode(GetEntityModel(PlayerPedId())),
+  }
+  FreezeEntityPosition(PlayerPedId(), true)
+  OldSkin = skin
+  Callback = cb
+  Cam.Create(PlayerPedId())
 end
 
-RegisterCommand("atl", function()
-	startAppearance({})
+RegisterCommand('atl', function()
+  startAppearance({}, function(skin)
+    print(skin)
+    if skin then
+      SetSkin(PlayerPedId(), skin, true)
+    end
+  end)
 end)
 
-exports("startAppearance", startAppearance)
+exports('startAppearance', startAppearance)
